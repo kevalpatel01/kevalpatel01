36dc36d4bdaf57cb152719d90b0ab6cca3ed46c8
keval patel <kevalpatel111999@gmail.com>
2021-07-29 20:30:32 +0530

    version 1

--- user/action/login.php
+++ user/action/login.php
@@ -5,31 +5,30 @@ include('classes/userclass.php');
 $login = new user();
 //public $conn;
 //echo "$login";exit;
-if(isset($_POST['login'])) {
+if (isset($_POST['login'])) {
     //session_start();
     $username = ($_POST['username']);
     $password = ($_POST['password']);
     //$_SESSION['login'] = true;
     //$_SESSION['username'] = "$username";
     //$_SESSION['password'] = "$password";
-    $result = $login->userlogin($username,$password);
+    $result = $login->userlogin($username, $password);
     //$login =$conn->result($userlogin);
     //echo "here";exit;
-        /*$_SESSION['login'] = true;
+    /*$_SESSION['login'] = true;
         $_SESSION['username'] = "$username";
         $_SESSION['password'] = "$password";*/
-    if($result) {
+    if ($result) {
         $_SESSION['login'] = true;
         $_SESSION['username'] = "$username";
         $_SESSION['password'] = "$password";
         header('location:index.php');
-    }
-    else {
-    
+    } else {
+
         $_SESSION['message'] = "Incorrect Login";
-       
+
         header('location:index.php');
-        
+
         //header('location:userlogin.php');
     }
 }
